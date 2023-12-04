d72813d423de4388dd55e7c6a7b311ad1ea066c3
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2023-12-04 18:39:03 +0100

    Containers plicado en el widget de profail para estructurar mejor

--- Project_Flutter/app_book/lib/Widgets/profile_widget.dart
+++ Project_Flutter/app_book/lib/Widgets/profile_widget.dart
@@ -1,54 +1,61 @@
 import 'package:flutter/material.dart';
-
 class ProfileWidget extends StatelessWidget {
-  const ProfileWidget({super.key});
+  const ProfileWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 50, // Ajusta la altura según tus necesidades
+            const SizedBox(height: 40),
+            const CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage('assets/icono.png'),
             ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Hello, Juan! ',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 95, 87, 87),
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'We wish you have a good day',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 136, 92, 92),
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+            const Align(
+              
+              alignment: Alignment.topCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Hello, Juan!',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 95, 87, 87),
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  Text(
+                    'We wish you have a good day',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 136, 92, 92),
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.blue,
                 ),
-                SizedBox(
-                  width: 120, // Ajusta el ancho según tus necesidades
+                SizedBox(width: 20), // Ajusta el espacio entre los contenedores según tus necesidades
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.green,
                 ),
-                  CircleAvatar(
-                  radius: 40,
-                  backgroundImage: AssetImage('assets/icono.png'),
-                ),
-               
               ],
             ),
           ],
