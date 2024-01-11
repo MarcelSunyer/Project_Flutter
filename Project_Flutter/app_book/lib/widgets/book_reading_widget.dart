2f87a50ec15c3c8073f861e79ece7ecdcdac14d9
Eric <ericluque2003@gmail.com>
2024-01-11 21:04:09 +0100

    Revert "Update book_reading_widget.dart"
    This reverts commit 5e4eaeda5987a704a15d61acc4dd25e83aacf313.

--- Project_Flutter/app_book/lib/widgets/book_reading_widget.dart
+++ Project_Flutter/app_book/lib/widgets/book_reading_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class BookReadingWidget extends StatelessWidget {
+  final double width;
+  final double height;
   final double borderRadius;
   final Color backgroundColor;
   final String text;
@@ -8,6 +10,8 @@ class BookReadingWidget extends StatelessWidget {
 
   const BookReadingWidget({
     Key? key,
+    this.width = 200.0,
+    this.height = 75.0,
     this.borderRadius = 38.0,
     this.backgroundColor = const Color.fromARGB(255, 67, 69, 70),
     required this.text,
@@ -16,42 +20,38 @@ class BookReadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final double width = constraints.maxWidth;
-
-        return Stack(
-          children: [
-            Positioned.fill(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(borderRadius),
-                  color: backgroundColor.withOpacity(0.5),
-                ),
-              ),
+    return Stack(
+      children: [
+        // Contenedor con blur
+        Positioned.fill(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              color: backgroundColor.withOpacity(0.5),
             ),
-            Container(
-              width: width,
-              margin: const EdgeInsets.all(5.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(borderRadius - 5),
-                color: backgroundColor,
+          ),
+        ),
+        Container(
+          width: width,
+          height: height,
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius - 5),
+            color: backgroundColor,
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(iconData, color: Colors.white),
+              Text(
+                text,
+                style: const TextStyle(color: Colors.white, fontSize: 22.0),
               ),
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(iconData, color: Colors.white),
-                  Text(
-                    text,
-                    style: const TextStyle(color: Colors.white, fontSize: 22.0),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
-      },
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
